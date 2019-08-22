695ad3f549735980bd18517d3908d8222c3c0f9b
cartes <cristiancartesa@gmail.com>
2019-08-22 16:39:21 -0400

    Middleware Api Auth

---
+++ api-rest-ahaus/app/Http/Middleware/ApiAuthMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ApiAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('Authorization');
+        $jwt = new JwtAuth();
+
+        $checkToken = $jwt->checkToken($token);
+
+        if ($checkToken) {
+
+            return $next($request);
+
+        } else {
+
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => __('Usuario no identificado')
+            ];
+
+            return response()->json($data, $data['code']);
+
+        }
+    }
+}

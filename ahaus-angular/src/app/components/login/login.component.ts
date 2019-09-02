2cc3f5485055181419cb5ee7e927cc1282359d3c
cristiancartes <Front_242>
2019-09-01 20:43:41 -0400

    First version of Login components

---
+++ ahaus-angular/src/app/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public page_title: string;
+
+  constructor() {
+    this.page_title = "Identifícate";
+  }
+
+  ngOnInit() {
+  }
+
+}

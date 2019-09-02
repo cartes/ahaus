fc398eab5607d86d50ac7990a0142d02dfddd152
cristiancartes <Front_242>
2019-09-01 21:51:21 -0400

    Componente de pagina de error

---
+++ ahaus-angular/src/app/components/error/error.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent implements OnInit {
+  public page_title: string;
+
+  constructor() {
+    this.page_title = "Página no encontrada";
+  }
+
+  ngOnInit() {
+  }
+
+}

40ed7b459c65aae62605a0a7a07b9714c3320456
cristiancartes <Front_242>
2019-09-01 21:46:32 -0400

    New dependencies

--- ahaus-angular/src/app/app.module.ts
+++ ahaus-angular/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { routing, appRoutingProvider} from "./app.routing";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {routing, appRoutingProvider} from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    routing
-  ],
-  providers: [
-      appRoutingProvider
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        routing
+    ],
+    providers: [
+        appRoutingProvider
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- ahaus-angular/src/app/app.routing.ts
+++ ahaus-angular/src/app/app.routing.ts
@@ -1,16 +1,18 @@
-import { ModuleWithProviders } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
 
 //Componentes
 
-import { LoginComponent } from "./components/login/login.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 //Rutas
 
 const appRoutes: Routes = [
-    { path: '', component: LoginComponent},
-    { path: 'inicio', component: LoginComponent},
-    { path: 'login', component: LoginComponent},
+    {path: '', component: LoginComponent},
+    {path: 'inicio', component: LoginComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
 
 

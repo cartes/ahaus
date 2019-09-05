5004985825ec0bcd51c4059a128d88866391b1ae
cartes <cristiancartesa@gmail.com>
2019-09-05 14:06:09 -0400

    Primera etapa login usuario

--- ahaus-angular/src/app/components/login/login.component.ts
+++ ahaus-angular/src/app/components/login/login.component.ts
@@ -1,21 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.scss'],
+    providers: [
+        UserService
+    ]
 })
 export class LoginComponent implements OnInit {
     public page_title: string;
     public user: User;
 
-    constructor() {
+    constructor(
+        private _userService: UserService
+    ) {
         this.page_title = "Identifícate";
         this.user = new User(1, '', '', '', '', '', '', '', '', 1, null, null, '');
     }
 
     ngOnInit() {
+    }
+
+    onSubmit(form) {
+        console.log(form);
+        form.reset();
     }
 
 }

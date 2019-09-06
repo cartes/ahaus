3cfc7f85f3fe794d55ec30c2bfbb854666349a13
cartes <cristiancartesa@gmail.com>
2019-09-06 14:27:47 -0400

    Correción

--- ahaus-angular/src/app/services/user.service.ts
+++ ahaus-angular/src/app/services/user.service.ts
@@ -2,17 +2,32 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../models/user";
+import {global} from "./global";
 
 @Injectable()
 export class UserService {
+    public url: string;
 
     constructor (
         public _http: HttpClient
     ) {
-
+        this.url = global.url;
     }
 
     test() {
         return "Hola Mundo desde el servicio";
+    }
+
+    signup(user, gettoken = null): Observable<any> {
+        if (gettoken != null) {
+            user.gettoken = true;
+        }
+
+        let json = JSON.stringify(user);
+        console.log(json);
+        let params = 'json='+json;
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoder');
+
+        return this._http.post(this.url+'user/login', params, {headers: headers});
     }
 }

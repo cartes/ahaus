953b55418eeaadc3a6d357755c6837e753622f19
cartes <cristiancartesa@gmail.com>
2019-09-10 15:19:55 -0300

    Actualizando ajustes de usuario, onSubmit para el formulario

--- ahaus-angular/src/app/components/user-edit/user-edit.component.ts
+++ ahaus-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
 
 @Component({
-  selector: 'app-user-edit',
-  templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.scss']
+    selector: 'app-user-edit',
+    templateUrl: './user-edit.component.html',
+    styleUrls: ['./user-edit.component.scss'],
+    providers: [
+        UserService
+    ]
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+    public page_title;
+    public user: User;
+    public identity;
+    public token;
+    public status;
 
-  ngOnInit() {
-  }
+    constructor(
+        private _userService: UserService
+    ) {
+        this.page_title = 'Ajustes de usuario';
+        this.user = new User(1, '', '', '', '', '', '', '', '', 1, null, null, '');
+
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+
+
+        this.user = this.identity;
+
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmit(form) {
+        console.log(this.user);
+        this._userService.update(this.token, this.user).subscribe(
+            response => {
+                console.log(response);
+            },
+            error => {
+                this.status = 'error';
+                console.log(<any>error);
+            }
+        )
+    }
 
 }

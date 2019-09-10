a1f08c19ae3b42fbc3a321483ab7af9f6f6121e3
cartes <cristiancartesa@gmail.com>
2019-09-10 15:21:06 -0300

    Metodo update creado, para acutliazcion de ajustes de usuario

--- ahaus-angular/src/app/services/user.service.ts
+++ ahaus-angular/src/app/services/user.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {User} from "../models/user";
 import {global} from "./global";
 
 @Injectable()
@@ -10,7 +9,7 @@ export class UserService {
     public identity;
     public token;
 
-    constructor (
+    constructor(
         public _http: HttpClient
     ) {
         this.url = global.url;
@@ -26,15 +25,25 @@ export class UserService {
         }
 
         let json = JSON.stringify(user);
-        let params = 'json='+json;
+        let params = 'json=' + json;
         let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 
-        return this._http.post(this.url+'user/login', params, {headers: headers});
+        return this._http.post(this.url + 'user/login', params, {headers: headers});
+    }
+
+    update(token, user) {
+        let json = JSON.stringify(user);
+        let params = "json=" + json;
+
+        console.log(token);
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('Authorization', token);
+
+        return this._http.put(this.url + "user/update", params, {headers: headers});
     }
 
     getIdentity() {
         let identity = JSON.parse(localStorage.getItem('identity'));
-        
+
         if (identity && identity != 'undefined') {
             this.identity = identity;
         } else {
@@ -43,9 +52,10 @@ export class UserService {
 
         return this.identity;
     }
+
     getToken() {
         let token = localStorage.getItem('token');
-        
+
         if (token && token != 'undefined') {
             this.token = token;
         } else {

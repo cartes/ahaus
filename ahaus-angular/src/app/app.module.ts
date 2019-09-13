5f013c573b90f3e21c6552ef040cca5d50082ef6
cartes <cristiancartesa@gmail.com>
2019-09-13 16:07:21 -0300

    Froala Editor y File Uploader

--- ahaus-angular/src/app/app.module.ts
+++ ahaus-angular/src/app/app.module.ts
@@ -2,13 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {routing, appRoutingProvider} from "./app.routing";
+import {appRoutingProvider, routing} from "./app.routing";
+import {FroalaEditorModule, FroalaViewModule} from "angular-froala-wysiwyg";
+import {AngularFileUploaderModule} from "angular-file-uploader";
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {ErrorComponent} from './components/error/error.component';
-import { UserEditComponent } from './components/user-edit/user-edit.component';
+import {UserEditComponent} from './components/user-edit/user-edit.component';
 
 @NgModule({
     declarations: [
@@ -22,7 +24,10 @@ import { UserEditComponent } from './components/user-edit/user-edit.component';
         BrowserModule,
         routing,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        FroalaEditorModule.forRoot(),
+        FroalaViewModule.forRoot(),
+        AngularFileUploaderModule
     ],
     providers: [
         appRoutingProvider

bd438736fb5fe27d8dd02bff3d048a5654965086
cartes <cristiancartesa@gmail.com>
2019-10-01 16:28:43 -0300

    Componente de comunidades

--- ahaus-angular/src/app/components/community-new/community-new.component.ts
+++ ahaus-angular/src/app/components/community-new/community-new.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
+import {CommunityService} from "../../services/community.service";
 import {Community} from "../../models/community";
 import {global} from "../../services/global";
 
 @Component({
   selector: 'app-community-new',
   templateUrl: './community-new.component.html',
-  styleUrls: ['./community-new.component.scss']
+  styleUrls: ['./community-new.component.scss'],
+  providers: [
+      CommunityService
+  ]
 })
 export class CommunityNewComponent implements OnInit {
+  public page_title: string;
+  public identity;
+  public token;
+  public community: Community;
 
-  constructor() { }
+  constructor(
+      private _route: ActivatedRoute,
+      private _router: Router,
+      private _communityService: CommunityService
+  ) {
+    this.community = new Community(1,'','','','','',1);
+  }
 
   ngOnInit() {
   }
